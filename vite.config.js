c105eccff51d99f52839ea94bfdb13dbe0672552
DearNoodle <wan619395@gmail.com>
2024-06-15 22:34:33 +0800

    feat: add gh-pages setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  /*
+  (eg. epository is at https://github.com/<USERNAME>/<REPO>),
+  then set
+  base: '/<REPO>/'
+  */
 });
